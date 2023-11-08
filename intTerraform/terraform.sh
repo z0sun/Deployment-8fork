153a944d52ff92e0e6ee0c15b0bbcb9d31d19ac8
Ubuntu <ubuntu@ip-172-31-13-214.ec2.internal>
2023-11-08 18:35:55 +0000

    Added setup files and jenkins infrastructure

---
+++ intTerraform/terraform.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo apt update 
+
+sudo apt install -y openjdk-11-jre
+
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+sudo apt update
+
+sudo apt-get install terraform
+
+sudo apt update 
+
+sudo install default-jre
